ffb9e2e5c3728be14f53d9386504b113b521e631
jay bhadoriya <18301@iiitu.ac.in>
2022-07-15 11:49:45 +0530

    Add project files.

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Console.ReadLine();
+
+
+        }
+    }
+}
